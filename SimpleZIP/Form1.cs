657b0dfc8f817c3f707f2d74312dfa43ce796c49
vmakar85@gmail.com <vmakar85@gmail.com@de14a5f7-d6c1-e796-01cd-d36adbcd66cc>
2010-09-09 21:31:08 +0000

    V 0.5 Не работает только выбор архивации папок целиком.

--- SimpleZIP/Form1.cs
+++ SimpleZIP/Form1.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 
 namespace SimpleZIP
 {
@@ -23,17 +26,57 @@ namespace SimpleZIP
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ikjh");
+            OpenFileDialog fileDialog1 = new OpenFileDialog();
+
+            fileDialog1.Title = "Выберите файл для архивации";
+            fileDialog1.Filter = "DarkBasicPro files (*.dba)|*.dba|All files (*.*)|*.*";
+            fileDialog1.FilterIndex = 1;
+            fileDialog1.RestoreDirectory = true;
+
+            if (fileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string zipFileName = fileDialog1.SafeFileName;
+                string zipFilePath__Name = fileDialog1.FileName;
+                addZipFile(zipFilePath__Name, zipFileName);
+                
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ikjh");
+            MessageBox.Show("Только в платной версии =P");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ikjh");
+            OpenFileDialog fileDialog1 = new OpenFileDialog();
+
+            fileDialog1.Title = "Распаковать фойл";
+            fileDialog1.Filter = "Simpl zip (*.zip)|*.zip|All files (*.*)|*.*";
+            fileDialog1.FilterIndex = 1;
+            fileDialog1.RestoreDirectory = true;
+
+            if (fileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string zipFileName = fileDialog1.SafeFileName;
+                UnZip(zipFileName, Directory.GetCurrentDirectory());
+            }
         }
+
+        public void addZipFile(string zipFilePath, string zipFileName)
+        {
+            FastZip fz = new FastZip();
+            Directory.CreateDirectory("Temp");
+            File.Copy(zipFilePath, "Temp\\" + zipFileName);
+            fz.CreateZip(zipFileName + ".zip", "Temp", true, null);
+            Directory.Delete("Temp", true);
+        }
+
+        public void UnZip(string zipFileName, string targetDir)
+        {
+            FastZip fz = new FastZip();
+            fz.ExtractZip(zipFileName, targetDir, null);
+        }
+
     }
 }
